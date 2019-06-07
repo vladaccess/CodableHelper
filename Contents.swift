2586804011c2f46590525207aa444a616afcdd08
Vlad <krasnokutskiy_v@utkonos.ru>
2019-06-07 12:21:48 +0300

    Init commit

---
+++ Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+extension KeyedDecodingContainer {
+    public func decode<T: Decodable>(_ type: T.Type = T.self,
+                                     key: Key, using formatter: DateFormatter) throws -> T? {
+        guard let stringValue = try decodeIfPresent(String.self, forKey: key) else {
+            return nil
+        }
+        return formatter.date(from: stringValue) as? T
+    }
+    
+    func decode<T: Decodable>(_ key: Key, as type:T.Type = T.self) throws -> T {
+        return try decode(T.self, forKey: key)
+    }
+    
+    func decodeIfPresent<T: Decodable>(_ key: Key, as type:T.Type = T.self) throws -> T? {
+        return try decodeIfPresent(T.self, forKey: key)
+    }
+}
